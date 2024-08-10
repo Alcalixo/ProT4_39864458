8240629db1a70d4671da4bde77d1c23bb0a41f9a
Alcalixo <rodrix_gonzalo@outlook.com>
2024-08-10 01:09:46 -0300

    Método UPDATE creado, funcionando correctamente

--- src/controller.js
+++ src/controller.js
@@ -22,5 +22,31 @@ class LibroController {
     );
     res.json({ "Id insertado": result.insertId });
   }
+
+  async delete(req, res) {
+    const libro = req.body;
+    const [result] = await pool.query(`DELETE FROM libros WHERE id_libro=(?)`, [
+      libro.id_libro,
+    ]);
+    res.json({ "Registros Eliminados": result.affectedRows });
+  }
+
+  async update(req, res) {
+    const libro = req.body;
+    const [result] = await pool.query(
+      `UPDATE libros SET titulo=?, autor=?, categoria=?, ano_publicacion=?, genero=?, sinopsis=?, isbn=? WHERE id_libro=?`,
+      [
+        libro.titulo,
+        libro.autor,
+        libro.categoria,
+        libro.ano_publicacion,
+        libro.genero,
+        libro.sinopsis,
+        libro.isbn,
+        libro.id_libro,
+      ]
+    );
+    res.json({ "Registros actualizado": result.changedRows });
+  }
 }
 export const libro = new LibroController();
